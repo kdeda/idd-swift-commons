2ec711d200608adc77f19ea8c1bb3997d1c65010
Klajd Deda <kdeda@id-design.com>
2023-04-08 12:08:46 -0400

    Pinning dependencies in preparation for v2

--- Package.swift
+++ Package.swift
@@ -10,27 +10,21 @@ let package = Package(
         .macOS(.v10_13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftCommons",
             targets: ["SwiftCommons"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kdeda/idd-log4-swift.git", from: "1.2.3"),
-        .package(url: "https://github.com/kdeda/idd-zstd-swift.git", from: "1.2.1"),
+        .package(url: "https://github.com/kdeda/idd-log4-swift.git", "1.2.5" ..< "2.0.0"),
+        .package(url: "https://github.com/kdeda/idd-zstd-swift.git", "1.2.5" ..< "2.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftCommons",
             dependencies: [
                 .product(name: "Log4swift", package: "idd-log4-swift"),
                 .product(name: "ZSTDSwift", package: "idd-zstd-swift")
             ]
-//            resources: [
-//                .copy("CenterToolBar/CenterToolBarHistory_activityButton-mask@2x.png")
-//            ]
         ),
         .testTarget(
             name: "SwiftCommonsTests",
